966ff95e50a84421b41c0c8c89488b048b837305
Nick <nick.chen@cmp.com.tw>
2023-07-07 14:44:21 +0800

    Add data REST support

--- src/main/java/tw/niq/example/repository/BeerRepository.java
+++ src/main/java/tw/niq/example/repository/BeerRepository.java
@@ -5,12 +5,12 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import tw.niq.example.entity.Beer;
 import tw.niq.example.model.BeerStyle;
 
-@Repository
+@RepositoryRestResource(path = "restBeers", collectionResourceRel = "beers")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
 	Page<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName, Pageable pageable);
